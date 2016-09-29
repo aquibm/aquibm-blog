4d78d523f278b50d3983629b93183a4abeea42dd
Aquib Master <aquibmaster@gmail.com>
2016-09-29 23:26:51 +1300

    Refactor template.jsx

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
+import React, { PropTypes } from 'react'
 
 import '../static/css/reset.css'
 import '../static/css/base.css'
 import '../static/css/typography.css'
 
-class Template extends React.Component {
-    render() {
-        const {location, children} = this.props
-
-        return (
-            <div className='wrapper'>
-              { children }
-            </div>
-		);
-    }
-}
+const Template = ({ children }) => {
+	return (
+		<div className="wrapper">
+			{ children }
+		</div>
+	);
+};
 
 Template.propTypes = {
-    children: React.PropTypes.any,
-    location: React.PropTypes.object,
-    route: React.PropTypes.object,
-}
+    children: PropTypes.object.isRequired
+};
 
-export default Template
+export default Template;
